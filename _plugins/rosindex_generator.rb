e4d9ce890a2e90904c3908298450f705e3254bff
Tully Foote <tullyfoote@intrinsic.ai>
2025-05-18 02:14:42 -0700

    Clean out the contributing suggestions which was mostly unused (#525)
    * Fix no rendering of issues and PRs if no CONTRIBUTING.md
    
    * Remove contribution_suggestions page
    
    * Remove unused generator functions
    Completes the pagination removal #522
    
    * remove unused contributions config
    
    * remove unused references to data['contributing']
    
    * Remove pagination js files and readme references
    
    * package_pager is usused now
    
    * remove trailing pager metadata too

--- _plugins/rosindex_generator.rb
+++ _plugins/rosindex_generator.rb
@@ -664,7 +664,6 @@ class Indexer < Jekyll::Generator
       'last_commit_time' => vcs.get_last_commit_time(),
       'readme' => nil,
       'readme_rendered' => nil,
-      'contributing' => nil,
       'contributing_rendered' => nil}
 
     # load the repo readme for this branch if it exists
@@ -672,7 +671,7 @@ class Indexer < Jekyll::Generator
       site, vcs.local_path, data['raw_uri'], data['browse_uri'])
 
     # load the repo CONTRIBUTING.md for this branch if it exists
-    data['contributing_rendered'], data['contributing'] = get_contributing(
+    data['contributing_rendered'] = get_contributing(
       site, vcs.local_path, data['raw_uri'], data['browse_uri'])
 
     unless repo.release_manifests[distro].nil?
@@ -846,92 +845,6 @@ class Indexer < Jekyll::Generator
 
   def generate_search_deps_list(site)
     site.pages << SearchDepsListPage.new(site)
-  end
- 
-  def generate_sorted_paginated(site, elements_sorted, default_sort_key, n_elements, elements_per_page, page_class)
-
-    n_pages = (n_elements / elements_per_page).floor + 1
-
-    (1..n_pages).each do |page_index|
-
-      p_start = (page_index-1) * elements_per_page
-
-      elements_sorted.each do |sort_key, elements|
-        # Get a subset of the elements
-        elements_sliced = Hash[
-          elements.collect do |distro, elements_in_distro|
-            [distro, elements_in_distro.slice(p_start, elements_per_page)]
-          end
-        ]
-        site.pages << page_class.new(site, sort_key, n_pages, page_index, elements_sliced)
-        # create page 1 without a page number or key in the url
-        if sort_key == default_sort_key and page_index == 1
-          site.pages << page_class.new(site, sort_key, n_pages, page_index, elements_sliced, true)
-        end
-      end
-    end
-  end
-
-  def sort_repos(site)
-    repos_sorted = {'name' => {}, 'time' => {}, 'released' => {}}
-
-    repos_sorted_by_name = @repo_names.sort_by { |name, _| name }
-    $all_distros.collect do |distro|
-      repos_sorted['name'][distro] = repos_sorted_by_name
-
-      repos_sorted['time'][distro] = \
-      repos_sorted['name'][distro].sort_by do |_, instances|
-        instances.default.snapshots.select do |d, s|
-          distro == d and not s.nil?
-        end.map do |d,s|
-          s.data['last_commit_time'].to_s
-        end.max.to_s
-      end.reverse
-
-      repos_sorted['released'][distro] = \
-      repos_sorted['name'][distro].sort_by do |_, instances|
-        instances.default.snapshots.count do |d, s|
-          d == distro and not s.nil? and s.released
-        end
-      end.reverse
-    end
-
-    return repos_sorted
-  end
-
-  def sort_repos_filtered(site, filter)
-    repos_sorted = {'name' => {}, 'time' => {}, 'doc' => {}, 'released' => {}}
-
-    repos_filtered = @repo_names.select { |key,_| filter.include? key }
-    repos_sorted_by_name = repos_filtered.sort_by { |name, _| name }
-    $all_distros.collect do |distro|
-      repos_sorted['name'][distro] = repos_sorted_by_name
-
-      repos_sorted['time'][distro] = \
-      repos_sorted['name'][distro].sort_by do |_, instances|
-        instances.default.snapshots.select do |d, s|
-          distro == d and not s.nil?
-        end.map do |d,s|
-          s.data['last_commit_time'].to_s
-        end.max.to_s
-      end.reverse
-
-      repos_sorted['doc'][distro] = \
-      repos_sorted['name'][distro].sort_by do |_, instances|
-        instances.default.snapshots.count do |d, s|
-          d == distro and not s.nil? and not s.data['readme'].nil?
-        end
-      end.reverse
-
-      repos_sorted['released'][distro] = \
-      repos_sorted['name'][distro].sort_by do |_, instances|
-        instances.default.snapshots.count do |d, s|
-          d == distro and not s.nil? and s.released
-        end
-      end.reverse
-    end
-
-    return repos_sorted
   end
 
   def write_release_manifests(site, repo, package_name, default)
@@ -1482,12 +1395,6 @@ class Indexer < Jekyll::Generator
     @rosdeps.each do |dep_name, full_dep_data|
       site.pages << DepPage.new(site, dep_name, raw_rosdeps[dep_name], full_dep_data)
     end
-
-    # create contribution suggestions list pages
-    puts ("Generating contribution suggestions list page...").blue
-
-    suggestions_sorted = sort_repos_filtered(site, site.config['contribute_suggested_repos'])
-    generate_sorted_paginated(site, suggestions_sorted, 'name', suggestions_sorted['name'].length, site.config['contributions_per_page'], ContributionSuggestionsPage)
 
     # populate the home page with available distros
     site.pages << HomePage.new(site)
